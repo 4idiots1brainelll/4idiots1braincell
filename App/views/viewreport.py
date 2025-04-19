127d2a2addb0b8bab27a085469e5606a5e7bf565
OgunSyntax <jaysenbarry@gmail.com>
2025-04-19 18:45:34 -0400

    smallfix

--- App/views/viewreport.py
+++ App/views/viewreport.py
@@ -3,10 +3,11 @@ from App.controllers import create_user, initialize
 
 from App.models import User
 
+from flask_jwt_extended import current_user, jwt_required, get_jwt_identity
 
 viewreport_views = Blueprint('viewreport_views', __name__, template_folder='../templates')
 
 @viewreport_views.route('/viewreport', methods=['GET'])
 def viewreport_page():
-    user = User.query.first()
+    user = current_user
     return render_template('viewreport.html', user=user)
